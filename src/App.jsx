af05069c2ee325968be0165a901e2f898d431b17
anushkapoonia07 <141112216+anushkapoonia07@users.noreply.github.com>
2023-11-24 13:39:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [item, setItem] = useState('');
+  const changeItem = (e) => {setItem(e.target.value);};
+  const addItemToList = () => {setTodos([...todos, item]);setItem('');};
+  const editItem = (index, e) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index] = e.target.value;
+    setTodos(updatedTodos);
+  };
+  const deleteItem = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className='CONTAINER'>
+      <div>
+        <input className='Text' type='text' placeholder='Type here' value={item} onChange={(e) => changeItem(e)}/>
+        <button type='submit' onClick={addItemToList}>Add Item</button>
+      </div>
+      <div>
+        {todos.map((todo, index) => (
+          <div key={index}>
+            <input type='text' className='Texts' value={todo} onChange={(e) => editItem(index, e)}/>
+            <button type='submit' onClick={() => deleteItem(index)}>Delete</button>
+          </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
